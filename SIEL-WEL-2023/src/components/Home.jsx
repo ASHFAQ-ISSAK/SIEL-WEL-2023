2f1395df4d4637e30eeb3177d7fc73a4fce03f59
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-07 10:43:38 +0300

    updated home

--- SIEL-WEL-2023/src/components/Home.jsx
+++ SIEL-WEL-2023/src/components/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
     <div className="mainnav">
       <div id="topbar">
         <div id="content">
-          <img src="/public/assets/152961aa-5e95-413b-b4bb-818dac96e7d4.svg" />
+          <img src="/assets/152961aa-5e95-413b-b4bb-818dac96e7d4.svg" />
           <h2 className="title">Street Insider Exposure League</h2>
           <h2 className="center">
             The league has 16 team grouped in two conferences NORTH and SOUTH.
